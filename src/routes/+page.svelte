91b48e0e4730f968b2ad599f197b025fedf5672f
ankit <ankitaabad.1@gmail.com>
2023-04-04 16:13:44 +0530

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<div class="container-md w-25  mt-4">
+
+  <form>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3 form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <div class="h-captcha" data-sitekey="5ce20e4e-a36c-43c9-8ee4-bd63ad94d8c7"></div>
+    <input type="submit" class="btn btn-primary" value="Submit">
+  </form>
+
+</div>
