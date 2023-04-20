106bb7e002f618c4dda2062224fe932feb8ce437
ankit <ankitaabad.1@gmail.com>
2023-04-20 10:42:50 +0530

    add recaptchav2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,4 +22,48 @@
 		rel="stylesheet"
 	/>
 </svelte:head>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- Container wrapper -->
+  <div class="container-fluid">
+    <!-- Toggle button -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <!-- Collapsible wrapper -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Navbar brand -->
+   
+      <!-- Left links -->
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Hcaptcha</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="recaptchav3">Recaptcha V3</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="recaptchav2">Recaptcha V2</a>
+        </li>
+      </ul>
+      <!-- Left links -->
+    </div>
+    <!-- Collapsible wrapper -->
+
+    <!-- Right elements -->
+
+    <!-- Right elements -->
+  </div>
+  <!-- Container wrapper -->
+</nav>
+<!-- Navbar -->
 <slot />
---
+++ src/routes/recaptchav2/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+	import { browser } from "$app/environment";
+ 
+  let token = ""
+  let copied = true
+  let form;
+  let verify_result
+  function onSubmit(input) {
+    console.log("get_token called", input)
+    token= input
+    navigator.clipboard.writeText(token).then(() => {
+      copied = false
+    })
+  }
+  if (browser) {
+    window.onSubmit = onSubmit;
+  }
+  
+
+</script>
+
+
+<div class="container-md w-25 mt-4">
+	<form bind:this={form}>
+		<div class="form-outline mb-3">
+      <input
+      type="email"
+      class="form-control"
+      id="exampleInputEmail1"
+      aria-describedby="emailHelp"
+			/>
+			<label for="exampleInputEmail1" class="form-label">Email address</label>
+		</div>
+		<div class="form-outline mb-3">
+      <input type="password" class="form-control" id="exampleInputPassword1" />
+			<label for="exampleInputPassword1" class="form-label">Password</label>
+		</div>
+		
+		
+		<div>token: {token}</div>
+    <div class:visually-hidden={copied}> copied</div>
+    <!-- <div>verify_result: {verify_result}</div> -->
+    <button class="g-recaptcha btn btn-primary" 
+    data-sitekey="6LcCHI0iAAAAAH_-L_BW49nb1CuPiuLAzg8UIGbo" 
+    data-callback='onSubmit' 
+    data-action='submit'>Submit</button>
+
+	</form>
+
+
+</div>
